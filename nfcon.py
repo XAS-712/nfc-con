05b7041e9acf5260294f7cfb93e7f2ed0f21c62c
XAS-712 <xas712@126.com>
2018-12-08 00:45:13 +0800

    Added FuncB

--- nfcon.py
+++ nfcon.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import os
 
 FUNC_TEXT=[
     "[A] Call \"nfc-list\" to show the card info.",
@@ -7,10 +8,49 @@ FUNC_TEXT=[
     "[C] Write Mifare Classic card with a dump file.",
     "[D] Format Mifare Classic card with the key file.",
     "[E] Crack half-encrypted Mifare classic card and export the key file.",
+    "[F] Reset Uid.",
 ]
+
+def Ls():
+    subprocess.call("ls -lAh mfd | grep \.mfd$",shell=True)
 
 def funcA():
     subprocess.call("nfc-list",shell=True)
+
+def funcB():
+    cmdline="nfc-mfclassic"
+    IsMCard=input("Is your card a magic card?(y/N)")
+    IsMCard=IsMCard.upper()
+    if IsMCard=="Y":
+        print("[Using magic card]")
+        cmdline = cmdline + " R"
+    else:
+        print("[Using normal card]")
+        cmdline = cmdline + " r"
+    KeyAB=input("Use KeyA or KeyB?(A/b)").upper()
+    if KeyAB == "B":
+        print("[Using KeyB]")
+        cmdline = cmdline + " b"
+    else:
+        print("[Using KeyA]")
+        cmdline = cmdline + " a"
+    cmdline = cmdline + " u"		#if your version does not contain this param, comment this line.
+    mfdName = input("Dump filename:(Enter for datetime-named)") + ".mfd"
+    if mfdName == ".mfd":
+        mfdName = time.strftime('%Y%m%d_%H:%M:%S',time.localtime(time.time())) + mfdName
+    cmdline = cmdline+" mfd/"+mfdName
+    if IsMCard != "Y":
+        Ls()
+        keyFile = input("You should provide a dump file with correct keys so that we can access the card.\nKey file:(without \".mfd\",Enter for default)")
+        if keyFile is not "":
+            keyFile="mfd/" +keyFile+".mfd"
+            if os.path.exists(keyFile):
+                cmdline=cmdline+" "+keyFile + " f" #use f to pass uid verify
+            else:
+                print("[Key file not found. Use default keys]")
+        else:
+            print("[Using default keys]")
+    subprocess.call(cmdline,shell=True)
 
 print("-"*24)
 print("#"*7 + "NFCConsole" + "#"*7)
@@ -23,12 +63,16 @@ while True:
     for functext in FUNC_TEXT:
         print(functext)
     print("-"*24)
-    choice=input("Select a function:(Q for quit) ")
+    choice=input("Select a function:(Q for quit,L for list dumps) ")
     choice=choice.upper()
     if choice=="Q":
         exit()
+    elif choice=="L":
+        Ls()
     elif choice=="A":
         funcA()
+    elif choice=="B":
+        funcB()
     else:
         print("Func not supported.")
         continue
